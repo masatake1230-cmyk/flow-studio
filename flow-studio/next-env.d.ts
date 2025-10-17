a66572d79e1cda8ba17369d715449a1d0ba58e0e
masatake1230-cmyk <masatake1230@gmail.com>
2025-10-18 01:56:08 +1100

    Delete flow-studio directory

--- flow-studio/next-env.d.ts
+++
@@ -1,3 +0,0 @@
-// This file is required by Next.js
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
--- flow-studio/public/sw.js
+++
@@ -1,8 +0,0 @@
-// Very minimal service worker to enable offline shell caching (optional)
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch', () => {});
